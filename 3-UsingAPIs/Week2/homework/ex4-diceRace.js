51f674faa4daf104f623f7ce86be09f544d4e582
Lydia Samir <L-Samir@hotmail.com>
2021-11-29 18:40:26 +0100

    delivering API homework week2

--- 3-UsingAPIs/Week2/homework/ex4-diceRace.js
+++ 3-UsingAPIs/Week2/homework/ex4-diceRace.js
@@ -3,29 +3,34 @@
 Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-UsingAPIs/Week2/README.md#exercise-4-dice-race
 
 1. Complete the function `rollTheDices()` by using `.map()` on the `dices` array 
-   to create an array of promises for use with `Promise.race()`.
+  to create an array of promises for use with `Promise.race()`.
 2. Refactor the function `main()` using async/await and try/catch.
 3. Once you got this working, you may observe that some dices continue rolling 
-   for some undetermined time after the promise returned by `Promise.race()` 
-   resolves. Do you know why? Add your answer as a comment to the bottom of the 
-   file.
+  for some undetermined time after the promise returned by `Promise.race()` 
+  resolves. Do you know why? Add your answer as a comment to the bottom of the 
+  file.
 ------------------------------------------------------------------------------*/
 // ! Do not remove this line
 const rollDice = require('../../helpers/pokerDiceRoller');
 
 function rollTheDices() {
   const dices = [1, 2, 3, 4, 5];
-  // TODO complete this function; use Promise.race() and rollDice()
+  const allDices = dices.map((dice) => rollDice(dice));
+  return Promise.race(allDices);
 }
 
 // Refactor this function to use async/await and try/catch
-function main() {
-  rollTheDices()
-    .then((results) => console.log('Resolved!', results))
-    .catch((error) => console.log('Rejected!', error.message));
+async function main() {
+  try {
+    const results = await rollTheDices();
+    console.log('Resolved!', results);
+  } catch (error) {
+    console.log('Rejected!', error.message);
+  }
 }
 
 main();
 
 // ! Do not change or remove the code below
 module.exports = rollTheDices;
+/*the array of promises will run anyway while promise race will give us the result of the faster or first ( resolved of rejected) promise */
